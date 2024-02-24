b5522825071c22cae54b6728e1a8c00dc9f0be5c
desaisa4 <sarthak-desai@live.com>
2024-02-24 16:17:42 -0400

    Initial Commit

---
+++ src/main/java/com/lca/LightsCameraAction/LightsCameraActionApplication.java
@@ -0,0 +1,13 @@
+package com.lca.LightsCameraAction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightsCameraActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LightsCameraActionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lca/LightsCameraAction/LightsCameraActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lca.LightsCameraAction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LightsCameraActionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
