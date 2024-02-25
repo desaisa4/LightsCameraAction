6ac6933000ccfa7d281869e464c695f8ddd5a377
desaisa4 <sarthak-desai@live.com>
2024-02-24 23:57:52 -0400

    Created a method to retreive movies currently playing

--- src/main/java/com/lca/LightsCameraAction/LightsCameraActionApplication.java
+++ src/main/java/com/lca/LightsCameraAction/LightsCameraActionApplication.java
@@ -2,12 +2,19 @@ package com.lca.LightsCameraAction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import com.lca.services.MovieServiceClient;
 
 @SpringBootApplication
 public class LightsCameraActionApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LightsCameraActionApplication.class, args);
+		
+		MovieServiceClient msc = new MovieServiceClient();
+		msc.getNowPlaying();
+		
 	}
-
+	
 }
